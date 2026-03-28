ea461907440890082d09fa2319f7dfe55652a31e
majhouldiscord2-debug <majhouldiscord2@gmail.com>
2026-03-28 17:07:45 +0100

    chore: aggressively ignore large internal directories to stay under GitHub 100 files limit

--- artifacts/discord-clone/src/hooks/useCurrentUser.ts
+++
@@ -1,7 +0,0 @@
-import { useAppStore } from "@/store/useAppStore";
-
-export function useCurrentUser() {
-  const userId = useAppStore((s) => s.currentUserId);
-  const users = useAppStore((s) => s.users);
-  return users[userId];
-}
